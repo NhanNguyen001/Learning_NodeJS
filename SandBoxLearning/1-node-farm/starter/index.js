057b5db3a004b806c51af48c8a1bdd8c233925ae
Nhan Nguyen Thanh <giup2151@gmail.com>
2019-07-09 17:50:30 +0700

    Sandbox Learning - Building a (Very) Simple API continued...

---
+++ SandBoxLearning/1-node-farm/starter/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview' ){
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product'){
+    res.end('This is the PRODUCT');
+  } else if (pathName === '/api') {
+
+    // Where the current file is located
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+      const productData = JSON.parse(data);
+      res.writeHead(200, {
+        'Content-type': 'application/json'
+      })
+      res.end(data);
+    })
+
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-word',
+    });
+    res.end('<h1>P age not found!</h1>')
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
