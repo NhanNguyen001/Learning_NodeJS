dbfdd14441d203c3dd6f64a3c874b84644987d48
Nhan <nhan2151@gmail.com>
2019-07-22 07:46:59 +0700

    Connecting Our Database with the Express App

--- SandBoxLearning/Working_Project_Natour_Project/server.js
+++ SandBoxLearning/Working_Project_Natour_Project/server.js
@@ -1,11 +1,28 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// console.log(app.get('env'));
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+// Deal with some deprecation warnings
+mongoose
+  .connect(DB, {
+    // .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    // console.log(con.connections);
+    console.log('DB connection successfully');
+  });
+
+// console.log(app.get('env'));
 const port = process.env.PORT || 2000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
