b6a8ea02798cf66bf1088e7aed7cadb98829f1a6
NhanNguyen001 <nhan2151@gmail.com>
2019-08-20 23:57:35 +0700

    Connecting Our Database with the Express App --- updated

--- SandBoxLearning/Working_Project_Natour_Project/server.js
+++ SandBoxLearning/Working_Project_Natour_Project/server.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({
+  path: './config.env'
+});
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -21,6 +23,37 @@ mongoose
     // console.log(con.connections);
     console.log('DB connection successfully');
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+// This is new document
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => console.log('ERROR', err));
 
 // console.log(app.get('env'));
 const port = process.env.PORT || 2000;
