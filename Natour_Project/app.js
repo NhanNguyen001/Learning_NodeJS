73fdf8403e825c04f809c56161f40b72ad056e32
Nhan Nguyen Thanh <giup2151@gmail.com>
2019-09-24 18:01:44 +0700

    Completed Implementing a Global Error Handling Middleware

--- Natour_Project/app.js
+++ Natour_Project/app.js
@@ -25,9 +25,27 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!!!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  // If next function recive argument, no matter what it is, Express will automatically know that there was an error
+  next(err);
+});
+
+// Using 4 parameters let express recognize this is a error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
