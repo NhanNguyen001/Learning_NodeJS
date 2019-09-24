10ee60b03041a6096f885f6ebf87484e9e32c1ed
NhanNguyen001 <nhan2151@gmail.com>
2019-09-24 23:24:45 +0700

    Completed Better Errors and Refactoring

--- Natour_Project/app.js
+++ Natour_Project/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -25,28 +27,11 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server!!!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
   // If next function recive argument, no matter what it is, Express will automatically know that there was an error
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!!!`, 404));
 });
 
 // Using 4 parameters let express recognize this is a error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
