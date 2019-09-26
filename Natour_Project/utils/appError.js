8546a4c5389e971b18a428eed8d4bb6d912f9dd5
NhanNguyen001 <nhan2151@gmail.com>
2019-09-26 22:02:29 +0700

    Update note file

--- Natour_Project/utils/appError.js
+++ Natour_Project/utils/appError.js
@@ -6,6 +6,9 @@ class AppError extends Error {
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
 
+    // When new obj is created and a constructor function is called
+    // then that function call is not gonna appear in the stack trace
+    // and will not pollute it
     Error.captureStackTrace(this, this.constructor);
   }
 }
