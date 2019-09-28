ee758e40273ef30750f665c108eff0710904c683
NhanNguyen001 <nhan2151@gmail.com>
2019-09-28 21:48:57 +0700

    Completed Handling Invalid Database IDs

--- Natour_Project/controllers/tourController.js
+++ Natour_Project/controllers/tourController.js
@@ -35,7 +35,9 @@ exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
 
   if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
+    return next(
+      new AppError(`No tour found with that ID (${req.params.id})`, 404)
+    );
   }
 
   res.status(200).json({
