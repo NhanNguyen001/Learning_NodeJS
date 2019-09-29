5d40a5207b8b4fd4f38cff8d23426a28568410c1
NhanNguyen001 <nhan2151@gmail.com>
2019-09-29 14:32:00 +0700

    Completed Catching Uncaught Exceptions - Completed Error Handling Session

--- Natour_Project/server.js
+++ Natour_Project/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({
   path: './config.env'
 });
@@ -28,9 +34,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-  console.log('error name: ', err.name);
-  console.log('error message: ', err.message);
   console.log('UNHANDLER REJECTION! Shutting down...');
+  console.log(err.name, err.message);
   // Close the server before exist process
   server.close(() => {
     process.exit(1);
