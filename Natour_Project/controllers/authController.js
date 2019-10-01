cb072c61392fd8bac01e6a79438981ac596b7470
Nhan Nguyen Thanh <giup2151@gmail.com>
2019-10-01 12:51:44 +0700

    Completed create new user

--- Natour_Project/controllers/authController.js
+++ Natour_Project/controllers/authController.js
@@ -1,7 +1,8 @@
 const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.signup = async (req, res, next) => {
-  const newUser = User.create({req.body});
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
 
   res.status(201).json({
     status: 'success',
@@ -9,4 +10,4 @@ exports.signup = async (req, res, next) => {
       user: newUser
     }
   });
-}
+});
