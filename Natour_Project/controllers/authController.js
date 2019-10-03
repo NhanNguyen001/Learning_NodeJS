22f1e9c0e858c043908a9f4b469e6dcd9ae34584
Nhan Nguyen Thanh <giup2151@gmail.com>
2019-10-03 21:44:21 +0700

    Logging User --- Ongoing

--- Natour_Project/controllers/authController.js
+++ Natour_Project/controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -21,5 +22,24 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser
     }
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  // 3) If everything is ok, send token to client
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token
   });
 });
